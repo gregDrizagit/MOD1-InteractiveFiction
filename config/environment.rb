99d8cf7fec8def4a93efdf099a45d93c66ce63ca
github username <github email address>
2017-11-14 19:18:48 -0500

    set up environment

---
+++ config/environment.rb
@@ -0,0 +1,13 @@
+require 'bundler/setup'
+Bundler.require
+
+require 'active_record'
+require 'rake'
+DB = ActiveRecord::Base.establish_connection({
+  :adapter => 'sqlite3',
+  :database => 'db/if_db.sqlite'
+}
+)
+
+
+require_all 'app'
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+room1 = Room.create(description:"This is the first room")
+room2 = Room.create(description:"This is the second room")
+room3 = Room.create(description:"This is the third room")
+user1 = User.create(name: "Greg")
